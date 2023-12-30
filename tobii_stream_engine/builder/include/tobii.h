e16dc1081ded27fb6332306bd08985b9e3d14c31
betaboon <betaboon@0x80.ninja>
2023-12-30 14:26:53 +0100

    chore: fix generated c header-files

--- tobii_stream_engine/builder/include/tobii.h
+++ tobii_stream_engine/builder/include/tobii.h
@@ -3,7 +3,7 @@ char const* tobii_error_message( tobii_error_t error );
 
 tobii_error_t tobii_get_api_version( tobii_version_t* version );
 
-tobii_error_t tobii_api_create( tobii_api_t** api, 
+tobii_error_t tobii_api_create( tobii_api_t** api,
     tobii_custom_alloc_t const* custom_alloc, tobii_custom_log_t const* custom_log );
 
 tobii_error_t tobii_api_destroy( tobii_api_t* api );
@@ -11,8 +11,8 @@ tobii_error_t tobii_api_destroy( tobii_api_t* api );
 tobii_error_t tobii_enumerate_local_device_urls( tobii_api_t* api,
     tobii_device_url_receiver_t receiver, void* user_data );
 
-tobii_error_t tobii_enumerate_local_device_urls_ex( tobii_api_t* api, 
-    tobii_device_url_receiver_t receiver, void* user_data, 
+tobii_error_t tobii_enumerate_local_device_urls_ex( tobii_api_t* api,
+    tobii_device_url_receiver_t receiver, void* user_data,
     uint32_t device_generations );
 
 tobii_error_t tobii_device_create( tobii_api_t* api, char const* url,
@@ -20,7 +20,7 @@ tobii_error_t tobii_device_create( tobii_api_t* api, char const* url,
 
 tobii_error_t tobii_device_destroy( tobii_device_t* device );
 
-tobii_error_t tobii_wait_for_callbacks( int device_count, tobii_device_t* const* devices )
+tobii_error_t tobii_wait_for_callbacks( int device_count, tobii_device_t* const* devices );
 
 tobii_error_t tobii_device_process_callbacks( tobii_device_t* device );
 
@@ -32,7 +32,7 @@ tobii_error_t tobii_update_timesync( tobii_device_t* device );
 
 tobii_error_t tobii_system_clock( tobii_api_t* api, int64_t* timestamp_us );
 
-tobii_error_t tobii_get_device_info( tobii_device_t* device, 
+tobii_error_t tobii_get_device_info( tobii_device_t* device,
     tobii_device_info_t* device_info );
 
 tobii_error_t tobii_get_track_box( tobii_device_t* device, tobii_track_box_t* track_box );
@@ -46,8 +46,8 @@ tobii_error_t tobii_get_state_uint32( tobii_device_t* device, tobii_state_t stat
 tobii_error_t tobii_get_state_string( tobii_device_t* device, tobii_state_t state,
     tobii_state_string_t value );
 
-tobii_error_t tobii_capability_supported( tobii_device_t* device, 
+tobii_error_t tobii_capability_supported( tobii_device_t* device,
     tobii_capability_t capability, tobii_supported_t* supported );
 
-tobii_error_t tobii_stream_supported( tobii_device_t* device, 
+tobii_error_t tobii_stream_supported( tobii_device_t* device,
     tobii_stream_t stream, tobii_supported_t* supported );
--- tobii_stream_engine/builder/include/tobii_advanced.h
+++ tobii_stream_engine/builder/include/tobii_advanced.h
@@ -2,12 +2,12 @@
 tobii_error_t tobii_gaze_data_subscribe( tobii_device_t* device,
     tobii_gaze_data_callback_t callback, void* user_data );
 
-tobii_gaze_data_unsubscribe( tobii_device_t* device );
+tobii_error_t tobii_gaze_data_unsubscribe( tobii_device_t* device );
 
 tobii_error_t tobii_digital_syncport_subscribe( tobii_device_t* device,
     tobii_digital_syncport_callback_t callback, void* user_data );
 
-tobii_digital_syncport_unsubscribe( tobii_device_t* device );
+tobii_error_t tobii_digital_syncport_unsubscribe( tobii_device_t* device );
 
 tobii_error_t tobii_enumerate_face_types( tobii_device_t* device, tobii_face_type_receiver_t receiver,
   void* user_data );
